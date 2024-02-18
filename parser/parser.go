31329de6639e631f87333febf10d3e3ea7602926
Cody Alan Stubbs <castubbs3@gmail.com>
2024-02-18 09:51:12 -0500

    Fix token spelling

--- parser/parser.go
+++ parser/parser.go
@@ -28,7 +28,7 @@ func (p *Parser) registerPrefix(tokenType token.TokenType, fn prefixParseFn) {
 	p.prefixParseFns[tokenType] = fn
 }
 
-func (p *Parser) registerInfix(tokenType toke.TokenType, fn infixParseFn) {
+func (p *Parser) registerInfix(tokenType token.TokenType, fn infixParseFn) {
 	p.infixParseFns[tokenType] = fn
 }
 
