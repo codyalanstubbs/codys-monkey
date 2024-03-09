0cbbb822d0d10ac4860328fde336578cea5dcc0b
Cody Alan Stubbs <castubbs3@gmail.com>
2024-03-09 09:43:22 -0500

    Add support for strings in the lexer
    Potential ideas to implement later:
    
    "If you think that this is too easy, feel free to make readString report an error instead of simply returning when it reaches the end of the input. Or you can add support for character escaping so that string literals like "hello \"world\"", "hello\n world" and "hello\t\t\tworld" work."

--- lexer/lexer_test.go
+++ lexer/lexer_test.go
@@ -23,7 +23,10 @@ func TestNextToken(t *testing.T) {
     }
 
     10 == 10;
-    10 != 9;`
+    10 != 9;
+    "foobar"
+    "foo bar"
+    `
 
 	tests := []struct {
 		expectedType    token.TokenType
@@ -102,7 +105,9 @@ func TestNextToken(t *testing.T) {
 		{token.NOT_EQ, "!="},
 		{token.INT, "9"},
 		{token.SEMICOLON, ";"},
-		// {token.EOF, ""},
+		{token.STRING, "foobar"},
+		{token.STRING, "foo bar"},
+		{token.EOF, ""},
 	}
 
 	l := New(input)
